94bca376bc5b2f7dedb329194063d1287fce94bc
Trevor Graham <graha662@umn.edu>
2020-05-28 14:16:59 -0500

    push

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+  newPost = 'No Content ';
+  enteredValue = ' ';
+  onAddPost() {
+    this.newPost = this.enteredValue;
+
+  }
+
+}
