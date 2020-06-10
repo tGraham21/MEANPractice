b8ec2869155061953b44c2d31aed500f9e82acb1
Trevor Graham <trevor.graham2121@gmail.com>
2020-06-10 15:01:59 -0500

    MongoDB connected to node

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose.connect("mongodb+srv://trevor:KTAfWyT0txoAxB26@cluster0-ozymd.mongodb.net/cluster0?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch(() =>{
+    console.log("Connection Failed :(");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +31,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully"
