dcb02d341a9328921dbfc1cf1fd5ef272cf00e90
Trevor Graham <trevor.graham2121@gmail.com>
2020-06-11 13:01:34 -0500

    fix delete bugs

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,3 +1,4 @@
+// Handles all http requests
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -35,10 +36,19 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>("http://localhost:3000/api/posts", post)
+    this.http.post<{message: string, postId: string}>("http://localhost:3000/api/posts", post)
       .subscribe((responseData) => {
-        console.log(responseData.message);
+        post.id = responseData.postId;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => (post.id !== postId));
+        this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
