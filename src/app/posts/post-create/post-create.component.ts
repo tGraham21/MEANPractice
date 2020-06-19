f0b0a756384f085e295098386ae6fe462395bc4f
Trevor Graham <trevor.graham2121@gmail.com>
2020-06-19 12:17:42 -0500

    spinner

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit{
   enteredContent = "";
   enteredTitle = "";
   post:  Post;
+  isLoading = false;
   private mode = 'create';
   private postId: string;
 
@@ -25,7 +26,9 @@ export class PostCreateComponent implements OnInit{
       if(paramMap.has('postId')){  // defined in app-routing,module.ts
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
+        this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
           this.post = {id: postData._id, title: postData.title, content: postData.content};
         });
       }
@@ -40,6 +43,7 @@ export class PostCreateComponent implements OnInit{
     if(form.invalid){
       return;
     }
+    this.isLoading = true;
     if(this.mode === 'create'){
     this.postsService.addPost(form.value.title, form.value.content);
     }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { Post } from './post.model';
 
@@ -11,7 +12,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http.get<{message: string, posts: any}>("http://localhost:3000/api/posts")
@@ -45,6 +46,7 @@ export class PostsService {
         post.id = responseData.postId;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
   }
 
@@ -57,6 +59,7 @@ export class PostsService {
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
 
   }
