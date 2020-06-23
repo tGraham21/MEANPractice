e12dc1007d27c2cccda6af3156fb2fc8acc22d36
Trevor Graham <trevor.graham2121@gmail.com>
2020-06-23 16:35:59 -0500

    image upload

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -38,7 +38,7 @@ export class PostCreateComponent implements OnInit{
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = {id: postData._id, title: postData.title, content: postData.content};
+          this.post = {id: postData._id, title: postData.title, content: postData.content, imagePath: null};
           this.form.setValue({    // set values of form group if editing
             'title': this.post.title,
             'content': this.post.content
@@ -69,7 +69,7 @@ export class PostCreateComponent implements OnInit{
     }
     this.isLoading = true;
     if(this.mode === 'create'){
-    this.postsService.addPost(this.form.value.title, this.form.value.content);
+    this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image as Blob);
     }
     else {
       this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
