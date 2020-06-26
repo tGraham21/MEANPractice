e33e8736ac7dcf287bb722b62b7d530907d625e3
Trevor Graham <trevor.graham2121@gmail.com>
2020-06-26 11:56:27 -0500

    fix image upload bugs

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -59,7 +59,8 @@ router.put("/:id", multer({ storage: storage }).single("image"),
   const post = new Post({
     _id: req.body.id,
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: imagePath
   });
   Post.updateOne({ _id: req.params.id }, post).then(result => {
     res.status(200).json({ message: "Update successful!" });
